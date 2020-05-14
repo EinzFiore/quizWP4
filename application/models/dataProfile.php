ca783da2552745a0a9c91e57cd779bc99e5c5ad4
Enjun <50412104+EinzFiore@users.noreply.github.com>
2020-05-14 15:34:16 +0700

    update model

--- application/models/dataProfile.php
+++ application/models/dataProfile.php
@@ -4,18 +4,6 @@ class dataProfile extends CI_Model
     function tambahData($data,$table){
         $this->db->insert($table,$data);
     }
-
-    function uploadFoto(){
-        
-        $config['upload_path'] = './uploads/';
-        $config['allowed_types'] = 'gif|jpg|png';
-        $config['max_size']  = '1024';
-        $config['overwrite'] = 'TRUE';
-        
-        $this->load->library('upload', $config);
-        $this->upload->do_upload('foto');
-        $data_foto = $this->upload->data('file_name');
-    }
 }
 
 
